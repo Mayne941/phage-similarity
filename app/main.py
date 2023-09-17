6419171203536f26a283c2451ebd7b58eac2f23d
Rich Mayne <mayne941@gmail.com>
2023-09-17 12:07:55 +0100

    chore: tidy, fnf error handling

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
 import networkx as nx
 import time
 from datetime import datetime
@@ -14,18 +13,7 @@ class GetPartitions:
     def __init__(self, params, is_louvain = False) -> None:
         '''Set up exp folder, decode runtime params'''
         self.is_louvain = is_louvain
-        self.le = LabelEncoder()
-        self.sample_size = params["sample_size"]
-        self.distance_value = params["distance_value"]    
-        self.input_file_path = params["input_file_path"]
-        self.taxonomy_file_path = params["taxonomy_file_path"]
-        self.save_partition_data = params["save_partition_data"]
-        self.plot_graph = params["plot_graph"]
-        self.dendro_threshold = params["dendro_threshold"]
-        self.trunc = params["dendro_truncation"]
-        self.dendro_width = params["dendro_width"]
-        self.hash_threshold = params["hash_threshold"]
-        self.p_threshold = params["p_threshold"]
+        self.params = params
         if not os.path.exists("./output/"):
             os.mkdir("./output/")
         self.fpath = f"./output/exp_{datetime.now().strftime('%d-%b-%Y--%H-%M-%S')}/"
@@ -34,16 +22,19 @@ class GetPartitions:
 
     def load_and_clean(self) -> pd.DataFrame:
         '''Read in data, filter duplicates and by p value threshold'''
-        df = pd.read_csv(self.input_file_path, delimiter="\t", engine="python", header=0, names=["binA", "binB", "distance", "p-value", "tX"]) 
+        try:
+            df = pd.read_csv(self.params["input_file_path"], delimiter="\t", engine="python", header=0, names=["binA", "binB", "distance", "p-value", "tX"]) 
+        except:
+            raise FileNotFoundError(f'Cant find your input file: {self.params["input_file_path"]}\nCheck that file exists and is in correct format')
         df_no_dups = df[~(df["binA"] == df["binB"])]
         '''Hash filter'''
         df_no_dups["t"] = df_no_dups["tX"].str.split("/").str[0].astype(int)
         df_no_dups["X"] = df_no_dups["tX"].str.split("/").str[1].astype(int)
         df_no_dups["hashes"] = df_no_dups["t"]/df_no_dups["X"]
         '''Sample size filter'''
-        if self.sample_size <= 0:
-            self.sample_size = df_no_dups.shape[0]
-        return df_no_dups.head(self.sample_size)
+        if self.params["sample_size"] <= 0:
+            self.params["sample_size"] = df_no_dups.shape[0]
+        return df_no_dups.head(self.params["sample_size"])
 
     def make_nx_graph(self, df) -> nx.Graph():
         '''Make NetworkX graph object'''
@@ -59,19 +50,19 @@ class GetPartitions:
         [gr.add_edges_from(i) for i in edges]
 
         '''Filter a list of edges from the graph that are greater than the distance threshold'''
-        long_edges = list(filter(lambda e: e[2] > self.distance_value, (e for e in gr.edges.data('distance'))))
+        long_edges = list(filter(lambda e: e[2] > self.params["distance_value"], (e for e in gr.edges.data('distance'))))
         le_ids = list(e[:2] for e in long_edges)
         gr.remove_edges_from(le_ids)
 
         '''Filter a list of edges from the graph that are greater than the p-value threshold'''
-        pval_edges = list(filter(lambda e: e[2] > self.p_threshold, (e for e in gr.edges.data('p-value'))))
+        pval_edges = list(filter(lambda e: e[2] > self.params["p_threshold"], (e for e in gr.edges.data('p-value'))))
         pval_ids = list(e[:2] for e in pval_edges)
         gr.remove_edges_from(pval_ids)
         print(nx.info(gr))
 
         ''' Write out a graphml file for visualisation in cytoscape'''
         nx.write_graphml(gr, f"{self.fpath}networkx.xml")
-        return gr, do_louvain_partitions(gr, self.save_partition_data, self.fpath)
+        return gr, do_louvain_partitions(gr, self.params["save_partition_data"], self.fpath)
 
     def make_i_graph(self, df) -> nx.Graph():
         '''Make NetworkX graph object'''
@@ -80,7 +71,7 @@ class GetPartitions:
         dist = df["distance"].tolist()
         hashes = df["hashes"].tolist()
         pvalue = df["p-value"].tolist()
-        df.to_csv(f"{self.fpath}passed_to_networkx.tsv", sep='\t', encoding='utf-8') # write out what is passed to the network
+        df.to_csv(f"{self.fpath}passed_to_networkx.tsv", sep='\t', encoding='utf-8') 
         gr = nx.Graph()
         [gr.add_node(i) for i in df["binA"].unique().tolist()]
         edges = [[(genome_names[i], binB[i], {"distance": dist[i], "hashes": hashes[i], "p-value": pvalue[i]})] for i in range(
@@ -88,18 +79,18 @@ class GetPartitions:
         [gr.add_edges_from(i) for i in edges]
 
         '''Filter a list of edges from the graph that are greater than the distance threshold'''
-        long_edges = list(filter(lambda e: e[2] > self.distance_value, (e for e in gr.edges.data('distance'))))
+        long_edges = list(filter(lambda e: e[2] > self.params["distance_value"], (e for e in gr.edges.data('distance'))))
         le_ids = list(e[:2] for e in long_edges)
         gr.remove_edges_from(le_ids)
 
         '''Filter a list of edges from the graph that are greater than the p-value threshold'''
-        pval_edges = list(filter(lambda e: e[2] > self.p_threshold, (e for e in gr.edges.data('p-value'))))
+        pval_edges = list(filter(lambda e: e[2] > self.params["p_threshold"], (e for e in gr.edges.data('p-value'))))
         pval_ids = list(e[:2] for e in pval_edges)
         gr.remove_edges_from(pval_ids)
         print(nx.info(gr))
         ''' Write out a graphml file for visualisation in cytoscape'''
         nx.write_graphml(gr, f"{self.fpath}networkx.xml")
-        return gr, do_leiden_partitions(gr, self.save_partition_data, self.fpath)
+        return gr, do_leiden_partitions(gr, self.params["save_partition_data"], self.fpath)
 
     def output_conditioning(self, df) -> pd.DataFrame():
         '''Produce structured data, save'''
@@ -115,7 +106,7 @@ class GetPartitions:
         output_table = pd.DataFrame()
         output_table["community_id"] = unique
         output_table["n_genomes"] = counts
-        output_table["threshold"] = self.distance_value
+        output_table["threshold"] = self.params["distance_value"]
         output_table.to_csv(f"{self.fpath}community_counts_table.csv")
         output_table["accession_ids"] = genomes
         output_table["accession_ids"] = output_table["accession_ids"].apply(lambda x: ", ".join(x))
@@ -124,20 +115,23 @@ class GetPartitions:
 
     def output_mapping(self, output_table) -> None:
         '''Map to current BVS taxonomy, save'''
-        # load the data
+        '''load the data & custom VMR with strain list'''
         communities_df = pd.DataFrame(output_table)
-        # Load custom version of the VMR that also lists strains
-        taxonomy_df = pd.read_csv(self.taxonomy_file_path, header=0)
-        # split the accession_ids field into lists
+        try:
+            taxonomy_df = pd.read_csv(self.params["taxonomy_file_path"], header=0)
+        except:
+            raise FileNotFoundError(f"Can't open your taxonomy file: {self.params['taxonomy_file_path']}\nCheck that it exists and is in csv format")
+        
+        '''split the accession_ids field into lists, explode the lists, merge and export'''
         communities_df['accession_ids'] = communities_df['accession_ids'].str.split(', ')
-        # explode the lists
         communities_df = communities_df.explode('accession_ids', ignore_index = True)
-        # merge and export
         mapped_results_df = pd.merge(communities_df, taxonomy_df[['GENBANK accession', 'Genus', 'Type']], left_on='accession_ids', right_on="GENBANK accession", how='left')
         mapped_results_df.to_csv(f"{self.fpath}mapped_taxa.csv", encoding='utf-8', index=False)
-        # Count the number of unique communities by genus name
+
+        '''Count the number of unique communities by genus name'''
         count_unique = mapped_results_df.groupby('Genus')['community_id'].nunique().reset_index(name='N_communities')
-        # Obtain a list of the community id(s) for each genus and export
+
+        '''Obtain a list of the community id(s) for each genus and export'''
         genus_communities_df = mapped_results_df.groupby('Genus')['community_id'].unique().apply(list).reset_index(name='community_ids')
         genus_communities_df = genus_communities_df.merge(count_unique[['Genus', 'N_communities']], left_on='Genus', right_on='Genus', how='left') 
         genus_communities_df.to_csv(f"{self.fpath}genus_community_ids.csv", encoding='utf-8', index=False)
@@ -162,24 +156,24 @@ class GetPartitions:
         output_table, p = self.output_conditioning(partition_df)
         self.output_mapping(output_table)
 
-        if self.plot_graph == True:
+        if self.params["plot_graph"] == True:
             '''Do dendrogram'''
             th = time.time()
-            if self.trunc == "lastp":
+            if self.params["dendro_truncation"] == "lastp":
                 dendro_labels = partition_df[0]
                 p_value = p
             else:
                 dendro_labels = partition_df.index
                 p_value = 0
-            create_dendrogram(partition_df, self.fpath, self.sample_size, self.dendro_threshold, self.trunc, self.dendro_width, p=p_value, labels=dendro_labels)
-            print(f"Dendrogram time, {self.sample_size} samples = {time.time()-th}")
+            create_dendrogram(partition_df, self.fpath, self.params["sample_size"], self.params["dendro_threshold"], self.params["dendro_truncation"], self.params["dendro_width"], p=p_value, labels=dendro_labels)
+            print(f"Dendrogram time, {self.params['sample_size']} samples = {time.time()-th}")
 
             # return # Only uncomment me if you really want to plot a massive network graph
 
             '''Do network graph'''
             pos = self.calculate_layout(graph_object)
             plot = ClusterPlot(
-                graph_object, partition_df[0], pos, self.sample_size, self.fpath)
+                graph_object, partition_df[0], pos, self.params["sample_size"], self.fpath)
             plot.make_all()
 
         msg = f"Finished in {round(time.time() - st, 2)} Sec. Results saved to {self.fpath}"
